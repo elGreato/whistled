01cb0024c9f6f24b1b7077ac844ad084d4d4ce92
Hyp-nos <ali.habbabeh@hotmail.com>
2020-03-20 17:57:04 +0100

    whistleblowers can buy and sell cases

--- client/src/contracts/Marketplace.sol
+++ client/src/contracts/Marketplace.sol
@@ -37,28 +37,66 @@ contract Marketplace {
         bool isPurchased
     );
 
-    function createCase(string memory _caseTitle,CaseType _caseType,string memory _caseDescribtion, uint256 _casePrice) public{
+    event CasePurchased(
+        uint256 caseId,
+        CaseType caseType,
+        string caseTitle,
+        string caseDescribtion,
+        uint256 casePrice,
+        address payable owner,
+        bool isPurchased
+    );
+
+    function createCase(string memory _caseTitle,CaseType _caseType,string memory _caseDescribtion,uint256 _casePrice) public {
+        //requires valid name and price
+        require(bytes(_caseTitle).length > 0, "Case Title is not valid!");
+        require(_casePrice > 0, "Case price is not valid!");
+
         caseCount++;
         //create the case
-        cases[caseCount] = Case(
-            caseCount,
-            _caseType,
-            _caseTitle,
-            _caseDescribtion,
-            _casePrice,
-            msg.sender,
-            false);
-            
+        cases[caseCount] = Case(caseCount,_caseType,_caseTitle,_caseDescribtion,_casePrice,msg.sender,false);
+
         //emit the event
-        emit CaseCreated(
-            caseCount,
-            _caseType,
-            _caseTitle,
-            _caseDescribtion,
-            _casePrice,
-            msg.sender,
-            false
-        );
+        emit CaseCreated(caseCount,_caseType,_caseTitle,_caseDescribtion,_casePrice,msg.sender,false);
 
     }
+
+
+    function purchaseCase(uint256 _caseId)public payable{
+
+        //fetch the case
+        Case memory _case = cases[_caseId];
+
+        //fetch the owner
+        address payable _seller = _case.owner;
+
+        //ensure case has a valid id
+        require(_case.caseId>0 && _case.caseId<=caseCount, "wrong case id");
+
+         //require that it has enough Ether
+        require(msg.value>=_case.casePrice, "not enough ether");
+
+        //require that's not prudhced
+        require(!_case.isPurchased, "case already purchsed");
+
+        //require that buyer is not seller
+        require(_seller != msg.sender, "can't buy your own cases");
+
+        //now we ensured all, transfer ownership and mark as Purchased
+        _case.owner = msg.sender;
+        _case.isPurchased = true;
+
+        //update the list
+        cases[_caseId] = _case;
+
+        //pay the seller
+        _seller.transfer(msg.value);
+
+        //trigger purchase event
+        emit CasePurchased(caseCount,_case.caseType,_case.caseTitle,_case.caseDescribtion,_case.casePrice,msg.sender,true);
+
+    }
+
+
+
 }
