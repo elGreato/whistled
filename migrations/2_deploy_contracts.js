0e7e48d1bfe62d7efe4d65f447314aa21caf458f
elGreato <ali.habbabeh@hotmail.com>
2020-06-05 09:36:04 +0200

    overwrite marketplace contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ const Marketplace = artifacts.require("Marketplace");
 const WhistledChat = artifacts.require("WhistledChat")
 
 module.exports = function (deployer) {
-  deployer.deploy(Marketplace, { overwrite: false });
+  deployer.deploy(Marketplace /* { overwrite: false } */);
   deployer.deploy(WhistledChat)
 };
